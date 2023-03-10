7c150f0c925eb28ea6f66f767c9720e58b8686af
SuperFranck <114146575+SuperFranck@users.noreply.github.com>
2023-03-10 09:36:54 +0100

    Add files via upload

--- GB/meshes.py
+++ GB/meshes.py
@@ -232,3 +232,78 @@ def generate_hollow_cylinder_2D(
         facet_tags.name = f"{msh.name}_facets"
         gmsh.finalize()
         return msh, cell_tags, facet_tags
+
+    
+
+
+def generate_PacMan(
+    R=1.,
+    theta=np.pi/4,
+    lc=0.1,
+    dist_min=0.1,
+    dist_max=0.3,
+    refinement_ratio=1,
+    gdim=2,
+    verbosity=10
+):
+    mesh_comm = MPI.COMM_WORLD
+    model_rank = 0
+    gmsh.initialize()
+    facet_tags = {"Top_lip": 3, "Bottom_lip": 2, "Disk": 1}
+    cell_tags = {"all": 20}
+    if mesh_comm.rank == model_rank:
+        model = gmsh.model()
+        model.add("PacMan")
+        model.setCurrent("PacMan")
+        # Create the points
+        h = R*np.tan(theta/2)
+        p1 = model.occ.addPoint(0.0, 0.0, 0, lc, 1)
+        p2 = model.occ.addPoint(-R, -h, 0, lc)
+        p3 = model.occ.addPoint(-R, h, 0, lc)
+        # Create the lines
+        l1 = model.occ.addLine(p1, p2)
+        l2 = model.occ.addLine(p2, p3)
+        l3 = model.occ.addLine(p3, p1)
+        # Create the surface
+        cloop1 = model.occ.addCurveLoop([l1, l2, l3])
+        surface_1 = model.occ.addPlaneSurface([cloop1])
+
+        disk = model.occ.addDisk(0, 0, 0, R, R)
+        whole_domain = model.occ.cut([(gdim, disk)], [(gdim, surface_1)])
+
+        # Define the mesh size and fields for the mesh refinement
+        model.mesh.field.add("Distance", 1)
+        model.mesh.field.setNumbers(1, "PointsList", [p3]) # p3 fonctionne mais en soit c'est juste qu'on ne connaît pas l'indice du centre du disque...
+                # SizeMax -                   / ------------------
+                #                            /
+                # SizeMin -o----------------/
+                #          |                |  |
+                #        Point        DistMin   DistMax
+        model.mesh.field.add("Threshold", 2)
+        model.mesh.field.setNumber(2, "IField", 1)
+        model.mesh.field.setNumber(2, "LcMin", lc / refinement_ratio)
+        model.mesh.field.setNumber(2, "LcMax", lc)
+        model.mesh.field.setNumber(2, "DistMin", dist_min)
+        model.mesh.field.setNumber(2, "DistMax", dist_max)
+        model.mesh.field.setAsBackgroundMesh(2)
+        model.occ.synchronize()
+        # Assign mesh and facet tags
+        surface_entities = [entity[1] for entity in model.getEntities(2)]
+        model.addPhysicalGroup(2, surface_entities, tag=cell_tags["all"])    
+        model.setPhysicalName(2, 2, "PacMan surface")
+
+        gmsh.option.setNumber('General.Verbosity', verbosity)
+        model.mesh.generate(gdim)
+
+        for (key, value) in facet_tags.items():
+                model.addPhysicalGroup(1, [value], tag=value)
+                model.setPhysicalName(1, value, key)
+
+        msh, cell_tags, facet_tags = gmshio.model_to_mesh(
+        model, mesh_comm, model_rank, gdim=gdim
+        )
+        gmsh.finalize()
+        msh.name = "PacMan"
+        cell_tags.name = f"{msh.name}_cells"
+        facet_tags.name = f"{msh.name}_facets"
+        return msh, cell_tags, facet_tags
